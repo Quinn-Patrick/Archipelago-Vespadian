d1ef4760882859a9631b3af7bd9cac0e036af788
Quinn-Patrick <quinn.bierbaum@gmail.com>
2025-07-12 17:12:06 -0400

    Complete __init__.py.

--- worlds/vespadian/__init__.py
+++ worlds/vespadian/__init__.py
@@ -1,26 +1,18 @@
 # world/mygame/__init__.py
 
-import settings
-import typing
-from .Items import VespadianItem, item_table  # data used below to add items to the World
-from .Locations import VespadianLocation, locationTable  # same as above
+from typing import List
+from .Items import VespadianItem, get_items_by_category, item_table, item_name_groups, event_item_table  # data used below to add items to the World
+from .Locations import location_table, location_name_groups  # same as above
 from .Options import VespadianOptions
+from .Regions import connect_entrances, create_regions
+from .Rules import set_rules
 from worlds.AutoWorld import World
-from BaseClasses import Region, Location, Entrance, Item, RegionType, ItemClassification
-
-class MyGameSettings(settings.Group):
-    class RomFile(settings.SNESRomPath):
-        """Insert help text for host.yaml here."""
-
-    rom_file: RomFile = RomFile("MyGame.sfc")
-
 
 class VespadianWorld(World):
     """Assemble your party of adventurers, delve dungeons, defeat monsters, collect loot, and travel to the distant past to defeat the Dread Sorcerer Ephemeris."""
     game = "Vespadian"  # name of the game/world
     options_dataclass = VespadianOptions  # options the player can set
     options: VespadianOptions  # typing hints for option results
-    settings: typing.ClassVar[MyGameSettings]  # will be automatically assigned from type hint
     topology_present = True  # show path to required location checks in spoiler
 
     # ID of first item and location, could be hard-coded but code may be easier
@@ -31,10 +23,57 @@ class VespadianWorld(World):
     # items exist. They could be generated from json or something else. They can
     # include events, but don't have to since events will be placed manually.
     item_name_to_id = {name: data.code for name, data in item_table.items()}
-    location_name_to_id = locationTable
+    location_name_to_id = {name: data.code for name, data in location_table.items()}
 
     # Items can be grouped using their names to allow easy checking if any item
     # from that group has been collected. Group names can also be used for !hint
-    item_name_groups = {
-        "weapons": {"sword", "lance"},
-    }
+    item_name_groups = item_name_groups
+
+    location_name_groups = location_name_groups
+    fillers = {}
+    fillers.update(get_items_by_category("Item"))
+    fillers.update(get_items_by_category("Equipment"))
+    fillers.update(get_items_by_category("Tome"))
+
+    def create_items(self):
+        item_pool: List[VespadianItem] = []
+        non_filler_item_categories = ["Key", "Strong Equipment", "Strong Tome"]
+
+        for name, data in item_table.items():
+            if data.category not in non_filler_item_categories:
+                continue
+            item_pool += [self.create_item(name)]
+        
+        total_locations = len(self.multiworld.get_unfilled_locations(self.player))
+
+        while len(item_pool) < total_locations:
+            item_pool.append(self.create_item(self.get_filler_item_name()))
+        
+        self.multiworld.itempool += item_pool
+
+
+    def create_item(self, name: str) -> VespadianItem:
+        data = item_table[name]
+        return VespadianItem(name, data.classification, data.code, self.player)
+
+    def create_event(self, name: str) -> VespadianItem:
+        data = event_item_table[name]
+        return VespadianItem(name, data.classification, data.code, self.player)
+
+    def set_rules(self):
+        print("Setting rules.")
+        print("Connected entrances: " + str(list(self.get_entrances())))
+        set_rules(self)
+
+    def create_regions(self):
+        print("Creating regions.")
+        create_regions(self.multiworld, self.player, self.options)
+        print("Done creating regions.")
+
+    def connect_entrances(self):
+        print("Connecting entrances.")
+        connect_entrances(self.multiworld, self.player)
+        print("Connected entrances: " + str(list(self.get_entrances())))
+        
+    def get_filler_item_name(self) -> str:
+        return self.random.choices([filler for filler in self.fillers.keys()])[0]
